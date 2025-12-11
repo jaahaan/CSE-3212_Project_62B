80c1a1900daaf87cf0b706f99b174c8724b2d251
Lisa <cse_1912020239@lus.ac.bd>
2025-12-11 10:18:43 +0600

    StatefulWidget, TextFormField

--- lib/profile_page.dart
+++ lib/profile_page.dart
@@ -1,15 +1,57 @@
 import 'package:flutter/material.dart';
 
-class ProfilePage extends StatelessWidget {
+class ProfilePage extends StatefulWidget {
   const ProfilePage({super.key});
 
+  @override
+  State<ProfilePage> createState() => _ProfilePageState();
+}
+
+class _ProfilePageState extends State<ProfilePage> {
+  TextEditingController nameController = TextEditingController();
+  String name = "";
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.blueGrey,
-        foregroundColor: Colors.white,
         title: Text("ProfilePage"),
+        backgroundColor: Colors.blueGrey,
+      ),
+      body: Center(
+        child: Container(
+          height: 300,
+          width: 300,
+          padding: EdgeInsets.all(20),
+          decoration: BoxDecoration(color: Colors.blueGrey[300]),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextFormField(
+                controller: nameController,
+                keyboardType: TextInputType.text,
+                // obscureText: true,
+                decoration: InputDecoration(
+                  labelText: "Name",
+                  hintText: "Enter Name",
+                  prefixIcon: Icon(Icons.person),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(50)),
+                  ),
+                ),
+              ),
+              SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () {
+                  name = nameController.text;
+                  setState(() {});
+                },
+                child: Text("Submit"),
+              ),
+              SizedBox(height: 20),
+              Text("Name is: $name"),
+            ],
+          ),
+        ),
       ),
     );
   }
